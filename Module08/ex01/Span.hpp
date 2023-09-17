7a4bfa36b8cdd18ae9d7e18c4b80ba378165eccf
Essadike elhafiane <saddik.bo@gmail.com>
2023-09-17 14:39:04 +0100

    finish

--- Module08/ex01/Span.hpp
+++ Module08/ex01/Span.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 
 class Span{
     private:
@@ -25,27 +26,31 @@ class Span{
         }
         void addNumber(int value)
         {
+            if (size <= list.size())
+                throw "Error the container is full !";
+            std::vector<int>::iterator it = std::find(list.begin(), list.end(), value);
+            if(it != list.end())
+                throw "Error element is found !";
             list.push_back(value);
             std::sort(list.begin(), list.end());
-            for (size_t i = 0; i < list.size(); ++i) {
-                std::cout << list[i] << " ";
-            }
-         std::cout << std::endl;
         }
-        void print()
-        {
-            for (size_t i = 0; i < list.size(); ++i) {
-                std::cout << list[i] << " ";
-            }
-         std::cout << std::endl;
-        }
+    
         int shortestSpan()
         {
-            
+            if (size == 0 || list.size() <= 1)
+                throw "Error lenght of containe is short !";
+            int shortspan = list[1] - list[0];
+            for (size_t i = 0; i < list.size() - 1; ++i) {
+                if (shortspan > list[i + 1] - list[i])
+                    shortspan = list[i + 1] - list[i];
+            }
+            return shortspan;
         }
         int longestSpan()
         {
-            return (list.end() - list.begin());
+            if (size == 0 || list.size() <= 1)
+                throw "Error lenght of containe is short !";
+            return (list[list.size() - 1] - list[0]);
         }
 
 };
--- Module08/ex01/main.cpp
+++ Module08/ex01/main.cpp
@@ -2,14 +2,19 @@
 
 int main()
 {
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    sp.print();
-    // std::cout << sp.shortestSpan() << std::endl;
-    // std::cout << sp.longestSpan() << std::endl;
+    Span sp = Span(10);
+    try
+    {
+        sp.addNumber(-1);
+        // sp.print();
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+        
+    }
+    catch(const char * s)
+    {
+        std::cerr << s << '\n';
+    }
+    
     return 0;
 }
