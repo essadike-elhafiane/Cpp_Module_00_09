7a22c32aec8f2976781af87a8c19ba21c6c5c48d
Essadike elhafiane <saddik.bo@gmail.com>
2023-09-21 20:31:58 +0100

    finish

--- Module09/ex01/main.cpp
+++ Module09/ex01/main.cpp
@@ -1,10 +1,33 @@
 #include "RPN.hpp"
+int check_valid_input(char *s)
+{
+    int degit;
+
+    degit = 0;
+    for (size_t i = 0; s[i]; i++)
+    {
+        if (s[i] == ' ')
+            degit = 0;
+        else
+        {
+            if (degit == 1)
+            {
+                std::cout << "Error input !" << std::endl;
+                return 1;
+            }
+            degit = 1;
+        }
+    }
+    return 0;
+}
 
 int main(int ac, char **av)
 {
     if (ac != 2)
         return (std::cout << "Error: bad arguments !" << std::endl, 1);
     RPN s;
+    if (check_valid_input(av[1]))
+        return 1;
     for (size_t i = 0; av[1][i]; i++)
     {
         if (av[1][i] == ' ')
